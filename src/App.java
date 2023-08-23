1db454c4c68cd37b63ab7cf4f5ba4facd946ce5f
BrunaCardoso7 <obitadrawing@gmail.com>
2023-08-23 20:20:11 -0300

    preparando o ambiente de desenvolvimento

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("my name is bruna");
+        System.out.println("i have 18 age");
+    }
+}
