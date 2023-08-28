19aea45a0a5cb5b46e8988b9d7dea0d39ef38c6a
BrunaCardoso7 <obitadrawing@gmail.com>
2023-08-28 20:47:14 -0300

    variaveis e métodos

--- src/App.java
+++ src/App.java
@@ -1,8 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("my name is bruna");
-        System.out.println("i have 18 age");
-        System.out.println("bunda");
+        final String BR = "Brasil";
+        System.out.println(BR);
+        String meuNome = "Bruna";
+        System.out.println(meuNome);
+        int minhaIdade = 18;
+        System.out.println(minhaIdade);
+        boolean goShowTwice = true;
+        System.out.println(goShowTwice + " vou para o show do twice");
+        goShowTwice = false;
+        System.out.println(goShowTwice + " não vou ao show do twice");
+        String TWICE = "9";
+        System.out.println(TWICE);
+        int num1 = 5;
+        int num2 = 4;
+        
+        int somar = somarNumero(num1, num2);
+
+        System.out.println("a soma dos números é " + somar);
+    }
+    public static int somarNumero(int num1, int num2){
+        return num1 + num2;
     }
 }
